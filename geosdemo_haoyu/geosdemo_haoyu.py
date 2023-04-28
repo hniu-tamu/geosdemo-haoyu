56b0b9b7e6e4f06a4c2de064d3a39d6d94bb641d
hniu <hniu@tamu.edu>
2023-04-28 17:53:12 -0500

    Added docstrings

--- geosdemo_haoyu/geosdemo_haoyu.py
+++ geosdemo_haoyu/geosdemo_haoyu.py
@@ -4,7 +4,16 @@ import string
 import random
 
 def get_random_string(length=10, upper=False, digits=False):
-    """Generate a random string of fixed length """
+    """Generate a random string of fixed length.
+
+    Args:
+        length (int, optional): The length of the string. Defaults to 10.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+
+    Returns:
+        str: The random string.
+    """    
     letters = string.ascii_lowercase
     if upper:
         letters = letters + string.ascii_uppercase
@@ -15,6 +24,13 @@ def get_random_string(length=10, upper=False, digits=False):
 
 
 def get_lucky_number(length=1):
-    """Generate a lucky number"""
+    """generate a random number of fixed length.
+
+    Args:
+        length (int, optional): The length of the number. Defaults to 1.
+
+    Returns:
+        int: the random number.
+    """    
     result = ''.join(random.choice(string.digits) for i in range(length))
     return int(result)
