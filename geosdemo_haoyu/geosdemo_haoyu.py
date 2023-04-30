f9516c6f6cca20632c61abed6cfb99cbe2eaa602
hniu <hniu@tamu.edu>
2023-04-30 18:04:47 -0500

    added layers

--- geosdemo_haoyu/geosdemo_haoyu.py
+++ geosdemo_haoyu/geosdemo_haoyu.py
@@ -13,6 +13,17 @@ class Map(ipyleaflet.Map):
             kwargs["scroll_wheel_zoom"] = True
         super().__init__(center=center, zoom=zoom, **kwargs)
 
+        if "layers_control" not in kwargs:
+            kwargs["layers_control"] = True
+        if kwargs["layers_control"]:
+            self.add_layers_control()
+
+        if "fullscreen_control" not in kwargs:
+            kwargs["fullscreen_control"] = True
+
+        if kwargs["fullscreen_control"]:
+            self.add_fullscreen_control()
+
     def add_search_control(self, position = "topleft", **kwargs):
         """Adds a search control to the map.
 
@@ -72,6 +83,64 @@ class Map(ipyleaflet.Map):
         }
 
         self.add_control(draw_control)
+
+
+    def add_layers_control(self, position = "topright"):
+        """Adds a layers control to the map.
+        
+        Args: Keyword arguments to pass to the layers control.
+        """
+
+        layers_control = ipyleaflet.LayersControl(position=position)
+        self.add_control(layers_control)
+
+    def add_fullscreen_control(self, position = "bottomright"):
+        """Adds a fullscreen control to the map.
+        
+        Args: Keyword arguments to pass to the fullscreen control.
+        """
+
+        fullscreen_control = ipyleaflet.FullScreenControl(position=position)
+        self.add_control(fullscreen_control)
+
+    def add_tile_layer(self, url, name, attribution="", **kwargs):
+        """Adds a tile layer to the map.
+        
+        Args:
+            url (str): The tile layer URL.
+            name (str): The tile layer name.
+            attribution (str): The tile layer attribution.
+            kwargs: The keyword arguments of ipyleaflet.TileLayer.
+        """
+
+        tile_layer = ipyleaflet.TileLayer(url=url, name=name, attribution=attribution, **kwargs)
+        self.add_layer(tile_layer)
+
+    def add_basemap(self, basemap, **kwargs):
+        """Adds a basemap to the map.
+        
+        Args:
+            basemap (str): The basemap name.
+            kwargs: The keyword arguments of ipyleaflet.TileLayer.
+        """
+        import xyzservices.providers as xyz
+
+        if basemap.lower() == "roadmap":
+            url = "http://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}"
+            self.add_tile_layer(url, name=basemap, **kwargs)
+        elif basemap.lower() == "satellite":
+            url = "http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}"
+            self.add_tile_layer(url, name=basemap, **kwargs)
+
+        else:
+            try:
+                basemap = eval(f"xyz.{basemap}")
+                url = basemap.build_url()
+                attribution = basemap.attribution
+                self.add_tile_layer(url, name=basemap.name, attribution=attribution, **kwargs)
+            except:
+                raise ValueError(f"Basemap '{basemap}' not found.")
+
 
 
 def get_random_string(length=10, upper=False, digits=False):
