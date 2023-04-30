08834404c7e74c5cdb0d8dc37370830ef7f18120
hniu <hniu@tamu.edu>
2023-04-30 16:19:36 -0500

    Add ipyleaflet

--- geosdemo_haoyu/geosdemo_haoyu.py
+++ geosdemo_haoyu/geosdemo_haoyu.py
@@ -12,6 +12,57 @@ class Map(ipyleaflet.Map):
             kwargs["scroll_wheel_zoom"] = True
         super().__init__(center=center, zoom=zoom, **kwargs)
 
+    def add_search_control(self, position = "topleft", **kwargs):
+
+        if "url" not in kwargs:
+            kwargs["url"] = "https://nominatim.openstreetmap.org/search?format=json&q={s}"
+
+        
+        search_control = ipyleaflet.SearchControl(position=position, **kwargs)
+        self.add_control(search_control)
+
+    def add_draw_control(self, position = "topleft", **kwargs):
+
+        if "edit" not in kwargs:
+            kwargs["edit"] = True
+
+        draw_control = ipyleaflet.DrawControl(position=position, **kwargs)
+        draw_control.polyline =  {
+            "shapeOptions": {
+                "color": "#6bc2e5",
+                "weight": 8,
+                "opacity": 1.0
+            }
+        }
+        draw_control.polygon = {
+            "shapeOptions": {
+                "fillColor": "#6be5c3",
+                "color": "#6be5c3",
+                "fillOpacity": 1.0
+            },
+            "drawError": {
+                "color": "#dd253b",
+                "message": "Oups!"
+            },
+            "allowIntersection": False
+        }
+        draw_control.circle = {
+            "shapeOptions": {
+                "fillColor": "#efed69",
+                "color": "#efed69",
+                "fillOpacity": 1.0
+            }
+        }
+        draw_control.rectangle = {
+            "shapeOptions": {
+                "fillColor": "#fca45d",
+                "color": "#fca45d",
+                "fillOpacity": 1.0
+            }
+        }
+
+        self.add_control(draw_control)
+
 
 def get_random_string(length=10, upper=False, digits=False):
     """Generate a random string of fixed length.
